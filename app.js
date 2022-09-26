e27085341b261139af674d75ffef99ae62e84a45
Neagu Tudor <tudorneagu7@gmail.com>
2022-09-27 00:04:54 +0300

    added the classes for the ui and book

---
+++ app.js
@@ -0,0 +1,30 @@
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+class UI {
+  static displayBooks() {
+    const books = store.getBooks();
+    books.forEach((book) => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn delete">X</a></td>
+    
+    `;
+
+    list.appendChild(row);
+  }
+}
